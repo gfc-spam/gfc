b39c6694fbd78e851463cce3e55d4d93c3fa9a4d
omarmom <57846393+omarmom@users.noreply.github.com>
2019-11-21 22:51:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,15 +12,6 @@ channel.send(process.env.MESSAGE);
 })
 
 client.on("ready", () => {
-    console.log(`Hi, ${client.user.username} is now online!`);
-
-    client.user.setPresence({
-        status: "online",
-        game: {
-            name: "Spam !",
-            type: "PLAYING"
-        }
-    }); 
 })
 
 client.on("message", async message => {
@@ -47,9 +38,6 @@ client.on("message", async message => {
         if (message.deletable) message.delete();
 
         if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
-        
-        // Role color
-        const roleColor = message.guild.me.highestRole.hexColor;
 
         // If the first argument is embed, send an embed,
         // otherwise, send a normal message
