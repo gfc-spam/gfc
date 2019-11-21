5a58363ed1db108f2e993b5149bb1217d913864a
omarmom <57846393+omarmom@users.noreply.github.com>
2019-11-21 20:30:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("⪧ SPAMMER ⪦");
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+let channel =     client.channels.get(process.env.CHANNEL)
+setInterval(function() {
+channel.send(process.env.MESSAGE);
+}, 30)
+})
+
+
 /*
 client.on("ready", () => {
 let channel =     client.channels.get(process.env.CHANNEL)
 setInterval(function() {
 channel.send(process.env.MESSAGE);
 }, 30)
-})*/
+})
 
 client.login(process.env.TOKEN);
 
