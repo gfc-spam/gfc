6afbffad45e6fc452a2e590e7ccc4e626f949b1a
omarmom <57846393+omarmom@users.noreply.github.com>
2019-11-21 17:52:51 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("⪧ SPAMMER ⪦");
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+
+const prefix = "-"
+
+let channel =     client.channels.get(process.env.CHANNEL)
+setInterval(function() {
+channel.send(process.env.MESSAGE);
+}, 30)
+})
+
+client.on("message", async message => {
+
+if(message.author.bot) return;
+if(message.content.indexOf(prefix) !== 0) return;
+
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage)
+}
+})
