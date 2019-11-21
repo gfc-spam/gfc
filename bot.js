edf8730d0ad213d5227c4a7996b0152688a37584
omarmom <57846393+omarmom@users.noreply.github.com>
2019-11-21 17:56:45 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on("message", async message => {
 if(message.author.bot) return;
 if(message.content.indexOf(prefix) !== 0) return;
 
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const args = message.content.slice(prefix.length).trim().split(' ');
 const command = args.shift().toLowerCase();
 
 if(command === "say") {
