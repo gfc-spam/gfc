dc5a1e3ab0023b2b7d74d9a2f694712d7d57779e
omarmom <57846393+omarmom@users.noreply.github.com>
2019-11-21 21:50:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,15 +11,17 @@ channel.send(process.env.MESSAGE);
 }, 30)
 })
 
-bot.on('ready', () => {
-    bot.user.setStatus('available')
-    bot.user.setPresence({
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    client.user.setPresence({
+        status: "online",
         game: {
-            name: 'As OMAR Said',
-            type: "Spamming",
+            name: "me getting developed",
+            type: "WATCHING"
         }
-    });
-});
+    }); 
+})
 
 client.on("message", async message => {
     const prefix = "-";
