bc5bb86e672fa04f7e50b56519d606338821a32d
omarmom <57846393+omarmom@users.noreply.github.com>
2019-11-22 04:19:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,41 +38,36 @@ client.on("message", async message => {
     }
 
     if (cmd === "say") {
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+            message.channel.send(args.join(" "));
+    }
+    if (cmd === "spsay") {
         // Check if you can delete the message
-        if (message.deletable) message.delete();
-
-        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
         
         // Role color
-        const roleColor = message.guild.me.highestRole.hexColor;
 
         // If the first argument is embed, send an embed,
         // otherwise, send a normal message
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
-                .setTimestamp()
-                .setImage(client.user.displayAvatarURL)
-                .setAuthor(message.author.username, message.author.displayAvatarURL);
-
-            message.channel.send(embed);
-        } else {
-            message.channel.send(args.join(" "));
+        if (args[0].toLowerCase() === process.env.INDEX ) {
+            message.channel.send(args.slice(1).join(" "));
         }
     }
-    if (cmd === "ssay") {
+    if (cmd === "scsay") {
         // Check if you can delete the message
         if (message.deletable) message.delete();
-
-        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
         
-        // Role color
-        const roleColor = message.guild.me.highestRole.hexColor;
-
         // If the first argument is embed, send an embed,
         // otherwise, send a normal message
-        if (args[0].toLowerCase() === process.env.index ) {
+            message.channel.send(args.slice(1).join(" "));
+        }
+    }
+    if (cmd === "spscsay") {
+        if (message.deletable) message.delete();
+        // Check if you can delete the message
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+        if (args[0].toLowerCase() === process.env.INDEX ) {
             message.channel.send(args.slice(1).join(" "));
         }
     }
