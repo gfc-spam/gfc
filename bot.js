4d68b1445e4b184c2d1dfd546090a93af83c4ded
omarmom <57846393+omarmom@users.noreply.github.com>
2019-11-22 04:48:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ console.log("⪧ SPAMMER ⪦");
 client.login(process.env.TOKEN);
 
 client.on("ready", () => {
-let channel =     client.channels.get(process.env.CHANNEL)
+let channel =     client.channels.get("646730428530556938")
 setInterval(function() {
-channel.send(process.env.MESSAGE);
+channel.send("| Loading Credits for gaming factor... | ...جارِ تحميل الكريديتس لجيمينج فاكتور |");
 }, 30)
 })
 
